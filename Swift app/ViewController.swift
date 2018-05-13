fcb637701023ce6679c32bac6b3cc3bd7dd2d331
Andrew <andyhughe@gmail.com>
2018-05-13 22:18:15 +0100

    Initial Commit

---
+++ Swift app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift app
+//
+//  Created by Andrew Hughes on 13/05/2018.
+//  Copyright © 2018 Andrew Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
